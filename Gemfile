f5ab51b0a167cd5b831e028ee5d26ad7688731e0
Tiago Lopes <tiago.delfim.lopes@gmail.com>
2017-11-28 23:11:11 +0000

    Model tests. System tests.

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'debase'
+  gem 'poltergeist'
   gem 'rubocop'
   gem 'ruby-debug-ide'
   gem 'selenium-webdriver'
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save article without title' do
+    article = Article.new
+    assert_not article.save
+
+    article.title = 'a'
+    assert_not article.save
+  end
+
+  test 'should not save article without text' do
+    article = Article.new
+    article.title = 'Dummy Title'
+    assert_not article.save
+
+    article.text = 'a'
+    assert_not article.save
+  end
+
+  test 'should save article' do
+    article = Article.new
+    article.title = 'Dummy Title'
+    article.text = 'Dummy Text'
+    assert article.save
+  end
 end
