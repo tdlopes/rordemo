eb05271ffea39d3365c57a0dec1c65f9fe45b376
Tiago Lopes <tiago.delfim.lopes@gmail.com>
2017-12-03 20:34:26 +0000

    Lograge, Sentry Raven.

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,22 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Rordemo
+  class CustomFormat < ::Logger::Formatter
+    # This method is invoked when a log event occurs
+    def call(severity, timestamp, progname, msg)
+      "[#{severity[0]}] [#{timestamp}] #{String === msg ? msg : msg.inspect}\n"
+    end
+  end
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    Raven.configure do |config|
+      config.dsn = 'https://9ace1ae1e8204c8b8dd5ad839fb1fcf2:f3609a3d22314a77b44cd9871fc03468@sentry.io/252894'
+    end
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = CustomFormat.new
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
